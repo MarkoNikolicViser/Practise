a1a38d27750ab9de9233576d5970423ca5f425ec
markonikolicviser <markonikolicviser@gmail.com>
2022-09-29 23:21:55 +0200

    useRef test

--- Typescript react-query types/src/components/Form.tsx
+++ Typescript react-query types/src/components/Form.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+  import React,{useRef} from 'react'
 import { useForm } from './useForm'
 
 export const Form = () => {
 const {mutation,onChange,onSubmit,inputData}=useForm()
+const passRef=useRef(null)
+console.log(passRef)
 
   return (
     <form onSubmit={onSubmit}>
         <input onChange={onChange} type="text" id='email' value={inputData.email} />
-        <input  onChange={onChange} type="password" id='password' value={inputData.password} />
+        <input ref={passRef} onChange={onChange} type="password" id='password' value={inputData.password} />
         <button type="submit">
             Submit This Form.
         </button>
