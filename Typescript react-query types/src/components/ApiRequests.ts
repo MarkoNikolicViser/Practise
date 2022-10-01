9e32f2f1cdc15621c81b529f3bf9238ebb839834
markonikolicviser <markonikolicviser@gmail.com>
2022-10-01 18:56:32 +0200

    first commit

--- Typescript react-query types/src/components/ApiRequests.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-import {LoginPayload,LoginData} from './Interfaces'
-
-const url='http://localhost:3001/'
-
-export const Login=(formData:LoginPayload)=>axios.post<LoginData>(`${url}user/login`,formData)
