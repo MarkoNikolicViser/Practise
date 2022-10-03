cadf358686484f85091dd6ee207ecce30d15edcf
markonikolicviser <markonikolicviser@gmail.com>
2022-10-03 22:18:17 +0200

    added a counter for tracking score

--- Typescript react-query types/src/App.tsx
+++ Typescript react-query types/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {toast} from 'react-toastify'
 
 export const App = () => {
+  const [counter,setCounter]=React.useState(0)
   const RandomColorGenerator=(min:number,max:number)=>{
     const randomNumber=Math.floor(Math.random() * (max - min + 1) + min)
     return `#FF${randomNumber}`
@@ -27,18 +28,22 @@ export const App = () => {
   }
   const buttonClick=(e: any)=>{
     const option=e.target.innerHTML
-    if(option===correctColor)
+    if(option===correctColor){
     toast.success('Correct color')
+      setCounter(prev=>prev+1)
+  }
     else{
       toast.error('Wrong color')
+      setCounter(0)
     }
   }
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center'>
-    <section style={{background:correctColor}} className={sectionStyle}>
-    </section>
+      <i className='text-xl pb-10'>Figure out the hex code for the color in the box</i>
+      <h1 className='font-medium text-2xl pb-5'>Score: {counter}</h1>
+    <section style={{background:correctColor}} className={sectionStyle}/>
     <div className='flex w-72 justify-between pt-5'>
-    {arr.map(color=><button onClick={buttonClick} className={buttonStyle} key={color}>{color}</button>)}
+    {arr.map((color:string)=><button onClick={buttonClick} className={buttonStyle} key={color}>{color}</button>)}
     </div>
     </div>
   )
